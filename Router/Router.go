d3195f8f6ec36822f44b4d64653b79f5ee540271
coaadasd <kimsunghoon122@gmail.com>
2024-03-29 11:58:00 +0900

    test

--- Router/Router.go
+++
@@ -1,7 +0,0 @@
-package Router
-
-import "github.com/labstack/echo/v4"
-
-func IndexHanddler(c echo.Context) error {
-	return c.File("")
-}
---
+++ src/main/java/frontend/src/pages/mainPage.js
@@ -0,0 +1,12 @@
+import Nav from "../component/nav";
+import Main from "../component/main";
+
+const MainPage = () => {
+  return(
+    <Nav>
+      <Main></Main>
+    </Nav>
+  )
+}
+
+export default MainPage;
