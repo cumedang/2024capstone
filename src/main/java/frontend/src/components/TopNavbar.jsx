0317972186f909c460b2bca820c9c1866f7125cb
byeongwoo <byeongwoo06@gmail.com>
2024-04-02 19:42:14 +0900

    first-commit

---
+++ src/main/java/frontend/src/components/TopNavbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const TopNavbar = () => {
+  return (
+    <div>
+      <div>LOGO</div>
+      <div>도서목록</div>
+      <div>채팅방</div>
+      <div>로그인</div>
+      <div>회원가입</div>
+    </div>
+  );
+};
---
+++ src/main/java/frontend/src/pages/MainPage.jsx
@@ -0,0 +1,13 @@
+import Main from "../components/Main";
+import { TopNavbar } from "../components/TopNavbar";
+
+const MainPage = () => {
+  return (
+    <>
+      <TopNavbar />
+      <Main />
+    </>
+  );
+};
+
+export default MainPage;
