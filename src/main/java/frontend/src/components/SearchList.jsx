2f15bb383040fc8f0e0f0024954b4d4cc043768d
coaadasd <kimsunghoon122@gmail.com>
2024-04-03 17:18:02 +0900

    리스트 출력 구현

--- src/main/java/frontend/src/components/SearchList.jsx
+++ src/main/java/frontend/src/components/SearchList.jsx
@@ -1,11 +1,22 @@
 import React, { useState, useRef, useEffect } from "react";
 import styles from "../styles/components/SearchList.module.css";
 import { SlArrowDown, SlArrowUp } from "react-icons/sl";
+import  axios  from "axios";
 
 const SearchList = () => {
   const [click, setClick] = useState(false);
   const [title, setTitle] = useState("제목");
+  const [keyword, isKeyword] = useState("");
+  const [data, setData] = useState([]);
   const modalWrapperRef = useRef(null);
+  const clickBoxRef = useRef(null);
+
+  useEffect(() => {
+    axios.get(`http://localhost:8000/books`)
+    .then(res => {
+      setData(res.data);
+    })
+  },[])
 
   const handleClickList = () => {
     setClick(prevState => !prevState)
@@ -17,7 +28,11 @@ const SearchList = () => {
   };
 
   const handleOutsideClick = (event) => {
-    if (modalWrapperRef.current && !modalWrapperRef.current.contains(event.target)) {
+    if (
+      modalWrapperRef.current &&
+      !modalWrapperRef.current.contains(event.target) &&
+      !clickBoxRef.current.contains(event.target)
+    ) {
       setClick(false);
     }
   };
@@ -34,10 +49,10 @@ const SearchList = () => {
       <div className={styles.ListContainer}>
         <div className={styles.searchContainer}>
           <div className={styles.search}>
-            <div className={styles.clickBox} onClick={handleClickList}>
+            <div className={styles.clickBox} ref={clickBoxRef} onClick={handleClickList}>
               {title}
               <div className={styles.icon}>
-                {click ? <SlArrowDown/> : <SlArrowUp />}
+                {click ? <SlArrowUp /> : <SlArrowDown/>}
               </div>
             </div>
             {click && (
@@ -48,10 +63,26 @@ const SearchList = () => {
                   <div className={styles.clickList} onClick={() => handleBoxClick('저자')}>저자</div>
               </div>
             )}
-            <input type="text" className={styles.inputText}></input>
-            <button className={styles.searchButton}>검색</button>
+            <input type="text" className={styles.inputText} value={keyword} onChange={(e) => isKeyword(e.target.value)}></input>
+            <button className={styles.searchButton} >검색</button>
           </div>
         </div>
+              <div className={styles.listContainer}>
+              <div className={styles.listContainer}>
+              {data.map((item, index) => (
+                <div key={index} className={styles.itemContainer}>
+                  <div className={styles.imgContainer}></div>
+                  <div>
+                    <div className={styles.list}>
+                      <div>{item.category}</div>
+                      제목: {item.title} 
+                      내용: {item.description}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>  
+              </div>
       </div>
     </div>
   );
