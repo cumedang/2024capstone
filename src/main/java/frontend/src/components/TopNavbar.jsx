e61c4db35716f4159a426dd67da4bf0f3bdc9e1a
byeongwoo <byeongwoo06@gmail.com>
2024-04-03 14:20:58 +0900

    third-commit

--- src/main/java/frontend/src/components/TopNavbar.jsx
+++ src/main/java/frontend/src/components/TopNavbar.jsx
@@ -4,11 +4,19 @@ import styles from "../styles/components/TopNavbar.module.css";
 const TopNavbar = () => {
   return (
     <div className={styles.container}>
-      <div>LOGO</div>
-      <div>도서목록</div>
-      <div>채팅방</div>
-      <div>로그인</div>
-      <div>회원가입</div>
+      <div className={styles.inner}>
+        <div className={styles.logo}>
+          <span>LOGO</span>
+        </div>
+        <div className={styles.menu}>
+          <span>도서목록</span>
+          <span>채팅방</span>
+        </div>
+        <div className={styles.util}>
+          <button className={styles.signup}>회원가입</button>
+          <button className={styles.login}>로그인</button>
+        </div>
+      </div>
     </div>
   );
 };
--- src/main/java/frontend/src/components/sessoin/Session1.jsx
+++ src/main/java/frontend/src/components/sessoin/Session1.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "../../styles/components/Session.module.css";
 
 const Session1 = () => {
-  return <div>Session1</div>;
+  return (
+    <div className={styles.center}>
+      <span>Session1</span>
+    </div>
+  );
 };
 
 export default Session1;
