20a0f502a3ea5fb32e098eac06a4183e06d494c9
byeongwoo <byeongwoo06@gmail.com>
2024-04-03 14:43:32 +0900

    fourth commit

--- src/main/java/frontend/src/components/TopNavbar.jsx
+++ src/main/java/frontend/src/components/TopNavbar.jsx
@@ -3,23 +3,32 @@ import styles from "../styles/components/TopNavbar.module.css";
 import { useNavigate } from "react-router-dom";
 
 const TopNavbar = () => {
-  const navigate = useNavigate()
-  const handleClickList = () => {
-    navigate("/search")
-  }
+  const navigate = useNavigate();
 
-  const handleClickLogo = () => {
-    navigate("/")
-  }
+  const navigatePage = (page) => {
+    navigate(`/${page}`);
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.inner}>
         <div className={styles.logo}>
-          <span onClick={() => {handleClickLogo()}}>LOGO</span>
+          <span
+            onClick={() => {
+              navigatePage("");
+            }}
+          >
+            LOGO
+          </span>
         </div>
         <div className={styles.menu}>
-          <span onClick={() => {handleClickList()}}>도서목록</span>
+          <span
+            onClick={() => {
+              navigatePage("search");
+            }}
+          >
+            도서목록
+          </span>
           <span>채팅방</span>
         </div>
         <div className={styles.util}>
