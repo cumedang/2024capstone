14894ec0bc1cc7e5bfb095b651d013d9bba18504
coaadasd <kimsunghoon122@gmail.com>
2024-04-03 15:31:29 +0900

    list page 만듦

--- src/main/java/frontend/src/components/SearchList.jsx
+++ src/main/java/frontend/src/components/SearchList.jsx
@@ -1,18 +1,55 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styles from "../styles/components/SearchList.module.css";
-import { SlArrowDown } from "react-icons/sl";
+import { SlArrowDown, SlArrowUp } from "react-icons/sl";
 
 const SearchList = () => {
   const [click, setClick] = useState(false);
-  const [title, setTitle] = useState("");
+  const [title, setTitle] = useState("제목");
+  const modalWrapperRef = useRef(null);
+
+  const handleClickList = () => {
+    setClick(prevState => !prevState)
+  }
+
+  const handleBoxClick = (type) => {
+    setTitle(type);
+    setClick(false);
+  };
+
+  const handleOutsideClick = (event) => {
+    if (modalWrapperRef.current && !modalWrapperRef.current.contains(event.target)) {
+      setClick(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOutsideClick);
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
+
   return (
     <div>
       <div className={styles.ListContainer}>
         <div className={styles.searchContainer}>
           <div className={styles.search}>
-            <div className={styles.clickBox}>제목 <div className={styles.icon}><SlArrowDown/></div></div>
+            <div className={styles.clickBox} onClick={handleClickList}>
+              {title}
+              <div className={styles.icon}>
+                {click ? <SlArrowDown/> : <SlArrowUp />}
+              </div>
+            </div>
+            {click && (
+              <div ref={modalWrapperRef} className={styles.clickListContainer}>
+                  <div className={styles.clickList} onClick={() => handleBoxClick('제목')}>제목</div>
+                  <div className={styles.clickList} onClick={() => handleBoxClick('내용')}>내용</div>
+                  <div className={styles.clickList} onClick={() => handleBoxClick('태그')}>태그</div>
+                  <div className={styles.clickList} onClick={() => handleBoxClick('저자')}>저자</div>
+              </div>
+            )}
             <input type="text" className={styles.inputText}></input>
-            <div></div>
+            <button className={styles.searchButton}>검색</button>
           </div>
         </div>
       </div>
